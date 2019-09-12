e31fd5dbbb96689b2503072e73348e14970809ab
David <david-metzler@web.de>
2019-09-12 19:58:00 +0200

    Fixed export errors

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Main extends Application {
@@ -20,7 +20,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         stage = primaryStage;
         // FXML-Datei laden!
 
@@ -63,16 +63,20 @@ public class Main extends Application {
         Parent root;
         try {
             loader.setController(controller);
-            root = loader.load(getClass()
-                    .getResourceAsStream(fxmlFile));
-            if (stage.getScene() == null) {
-                this.stage.setScene(new Scene(root, 760, 400));
-            } else {
-                this.stage.getScene().setRoot(root);
+            InputStream is = Main.class.getResourceAsStream(fxmlFile);
+            if (is != null) {
+                root = loader.load(is);
+                if (stage.getScene() == null) {
+                    this.stage.setScene(new Scene(root, 760, 400));
+                } else {
+                    this.stage.getScene().setRoot(root);
+                }
+                this.stage.getScene().getStylesheets().add(Main.class.getResource("/stylesheets/layout.css").toExternalForm());
+            }else {
+                System.out.println("Could not locate fxml: " + fxmlFile);
             }
-            this.stage.getScene().getStylesheets().add(getClass().getResource("/stylesheets/layout.css").toExternalForm());
-        } catch (IOException e) {
-            e.printStackTrace();
+        }catch (Exception e2){
+            e2.printStackTrace();
         }
 
     }
